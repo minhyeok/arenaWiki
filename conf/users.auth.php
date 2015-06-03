b1d3f0e87e57b243a52e7ec037bde3ecece335c6
root <root@intoarena1.cafe24.com>
2015-06-03 16:27:32 +0900

    backuped commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+intoarena:$1$7xhFuT1Y$Q3hlRxBjKUIGNWviXLO8V/:이민혁:intoarena@gmail.com:admin,user
